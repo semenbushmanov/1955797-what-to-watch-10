839c0c8f4130449209a178e405d6cc8e0ca95cd4
Semen Bushmanov <bush.sem@mail.ru>
2022-08-16 16:19:12 +0500

    add switching of header depending on authorization status

--- project/src/components/user-block/user-block.tsx
+++ project/src/components/user-block/user-block.tsx
@@ -1,15 +1,29 @@
+import { useAppSelector } from '../../hooks';
+import { AuthorizationStatus, AppRoute } from '../../const';
+import { Link } from 'react-router-dom';
+
 function UserBlock(): JSX.Element {
+  const { authorizationStatus } = useAppSelector((state) => state);
+
+  if ( authorizationStatus === AuthorizationStatus.Auth) {
+    return (
+      <ul className="user-block">
+        <li className="user-block__item">
+          <div className="user-block__avatar">
+            <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+          </div>
+        </li>
+        <li className="user-block__item">
+          <Link to={AppRoute.Main} className="user-block__link">Sign out</Link>
+        </li>
+      </ul>
+    );
+  }
+
   return (
-    <ul className="user-block">
-      <li className="user-block__item">
-        <div className="user-block__avatar">
-          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-        </div>
-      </li>
-      <li className="user-block__item">
-        <a href="#todo" className="user-block__link">Sign out</a>
-      </li>
-    </ul>
+    <div className="user-block">
+      <Link to={AppRoute.SignIn} className="user-block__link">Sign in</Link>
+    </div>
   );
 }
 
